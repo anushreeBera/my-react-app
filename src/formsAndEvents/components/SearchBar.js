377900a6eff48cfe816cf7fd1ba98f177dc5e91a
Anushree Bera <anushreejsr@gmail.com>
2022-07-14 06:32:19 +0000

    Controlled component

--- src/formsAndEvents/components/SearchBar.js
+++ src/formsAndEvents/components/SearchBar.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" />
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input type="text" 
+                            value={this.state.term}
+                            onChange={(event) => this.setState({term: event.target.value})}
+                        />
+                    </div>
                 </form>
             </div>
         );
