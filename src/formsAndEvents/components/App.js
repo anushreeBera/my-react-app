88dfa4a78fe64846865f6f6d939fb18791a44243
Anushree Bera <anushreejsr@gmail.com>
2022-08-08 10:57:22 +0000

    add axios, make API call

--- src/formsAndEvents/components/App.js
+++ src/formsAndEvents/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return <div className="ui container" style={{marginTop: '10px'}}>
-    <SearchBar />
-    </div>;
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {query: term},  
+      headers : {
+          Authorization: 'Client-ID omnZhGDsrJKOsJaOmhB6_iQX_KWjoeXdkPhv_DQ8Fgo'
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 };
 
 export default App;
